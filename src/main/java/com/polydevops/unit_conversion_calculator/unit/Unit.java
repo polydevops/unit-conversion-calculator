e4a51444e21321423cee0bb710ca2f527bcbe51c
Connor Pauly <cp1992@gmail.com>
2016-09-07 15:21:06 -0700

    Modified logic so that InvalidConversionException is thrown on the client side, therefore the client can handle invalid conversions (for example, you can't convert from grams (mass) -> liters (volume))

--- src/main/java/com/polydevops/unit_conversion_calculator/unit/Unit.java
+++ src/main/java/com/polydevops/unit_conversion_calculator/unit/Unit.java
@@ -12,63 +12,63 @@ public abstract class Unit {
     }
 
     public abstract String getUnitType();
+
     public abstract String getUnitGroup();
 
     public abstract Gram convertToGrams() throws InvalidConversionException;
+
     public abstract Ounce convertToOunces() throws InvalidConversionException;
 
     public abstract Milliliter convertToMilliliters() throws InvalidConversionException;
+
     public abstract Liter convertToLiters() throws InvalidConversionException;
 
     public abstract Teaspoon convertToTeaspoons() throws InvalidConversionException;
+
     public abstract Tablespoon convertToTablespoons() throws InvalidConversionException;
+
     public abstract Cup convertToCups() throws InvalidConversionException;
+
     public abstract Pint convertToPints() throws InvalidConversionException;
+
     public abstract Quart convertToQuarts() throws InvalidConversionException;
+
     public abstract FluidOunce convertToFluidOunces() throws InvalidConversionException;
 
     public double getValue() {
         return value;
     }
 
-    public Unit convertTo(java.lang.String toUnit) {
-        final java.lang.String fromUnit = getUnitType();
+    public Unit convertTo(java.lang.String toUnit) throws InvalidConversionException {
+        final String fromUnit = getUnitType();
 
         if (fromUnit.equals(toUnit)) {
             return this;
         }
 
-        try {
-            switch (toUnit) {
-                case UnitType.GRAM:
-                    return convertToGrams();
-                case UnitType.OUNCE:
-                    return convertToOunces();
-                case UnitType.MILLILITER:
-                    return convertToMilliliters();
-                case UnitType.LITER:
-                    return convertToLiters();
-                case UnitType.TEASPOON:
-                    return convertToTeaspoons();
-                case UnitType.TABLESPOON:
-                    return convertToTablespoons();
-                case UnitType.CUP:
-                    return convertToCups();
-                case UnitType.PINT:
-                    return convertToPints();
-                case UnitType.QUART:
-                    return convertToQuarts();
-                case UnitType.FLUID_OUNCE:
-                    return convertToFluidOunces();
-                default:
-                    return null;
-            }
-        } catch (InvalidConversionException e) {
-            // TODO
-            e.printStackTrace();
-            return null;
+        switch (toUnit) {
+            case UnitType.GRAM:
+                return convertToGrams();
+            case UnitType.OUNCE:
+                return convertToOunces();
+            case UnitType.MILLILITER:
+                return convertToMilliliters();
+            case UnitType.LITER:
+                return convertToLiters();
+            case UnitType.TEASPOON:
+                return convertToTeaspoons();
+            case UnitType.TABLESPOON:
+                return convertToTablespoons();
+            case UnitType.CUP:
+                return convertToCups();
+            case UnitType.PINT:
+                return convertToPints();
+            case UnitType.QUART:
+                return convertToQuarts();
+            case UnitType.FLUID_OUNCE:
+                return convertToFluidOunces();
+            default:
+                return null;
         }
     }
-
-
 }
